c79159140004223662b7a2f5144af0a9c4c01267
Daniel Silverman <daniel.silverman@me.com>
2019-04-26 12:46:14 -0700

    added data and engine files to its own branch

---
+++ engine/data.py
@@ -0,0 +1,12 @@
+"""
+This file is for all the data
+use lists with key (name) and value (description)
+"""
+
+#engine array
+
+#verb array
+
+#room array
+
+#item array
---
+++ engine/engine.py
@@ -0,0 +1,12 @@
+"""
+engine file starts the game
+and progresses the story
+"""
+
+print("welcome to poe's adventure!")
+
+#init data
+#init room
+#init npcs
+#init items
+#init parser
