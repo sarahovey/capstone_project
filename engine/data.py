0392755a731b73056769add8fa65475a24be5a15
Daniel Silverman <daniel.silverman@me.com>
2019-04-26 14:54:37 -0700

    fleshing out engine and data plans

--- engine/data.py
+++ engine/data.py
@@ -4,9 +4,23 @@ use lists with key (name) and value (description)
 """
 
 #engine array
+engine = {}
+#store flagging?
 
 #verb array
+verbs = {}
+actions = {}
+#can we change to actions please, verbs is so done
 
 #room array
+rooms = {}
+backyard = {}
+library_office = {}
+bedroom = {}
+kitchen = {}
+living_room = {}
 
 #item array
+items = {}
+inventory = {}
+quest_items = {}
--- engine/engine.py
+++ engine/engine.py
@@ -1,12 +1,34 @@
 """
 engine file starts the game
 and progresses the story
-"""
-
-print("welcome to poe's adventure!")
-
-#init data
+#init data/objects?
 #init room
 #init npcs
 #init items
 #init parser
+"""
+
+#import all files because the engine will use them all
+import * from data
+import * from command_parser
+import * from engine
+
+print("welcome to poe's adventure!")
+print("press the any key to continue")
+
+while(True):
+    #run the game
+    #imports cover initial initializing
+    """
+    1. load the game
+        a) load room
+        b) load npcs
+        c) load items
+    d) once that is taken care of 
+    
+    2. player actions
+        a) init command parser to read input
+        b) command parser directs data and engine
+        c) engine runs whatever command parser does
+    3. finish game
+    """
