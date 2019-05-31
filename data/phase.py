75887dc5530e2d4c2cface04136f6eb8e550db4a
Sara <sara.hovey@gmail.com>
2019-05-31 23:15:46 +0000

    add support for event driven room entering, and add some new items to the rooms

--- data/phase.py
+++ data/phase.py
@@ -14,26 +14,28 @@ class Phase1:
         living_room = rooms.living_room(self.player)
         self.rooms = [living_room]
         self.player.change_room(living_room)
-        #print(self.player.current_room.name)
         self.player.current_room.enter_room()
-        # kitchen = rooms.kitchen()
-        # kitchen_door = items.door(self.player, "kitchen key", living_room, kitchen)
-        # kitchen.doors = [kitchen_door]
         
-        # back_yard = rooms.back_yard()
-        # back_yard_door = items.door(self.player, "none", living_room, back_yard)
-        # back_yard.doors = [back_yard_door]
+        kitchen = rooms.kitchen()
+        kitchen_door = items.door(self.player, "kitchen key", living_room, kitchen)
+        kitchen.doors = [kitchen_door]
+        
+        back_yard = rooms.back_yard()
+        back_yard_door = items.door(self.player, "none", living_room, back_yard)
+        back_yard.doors = [back_yard_door]
         
         
-        # bedroom = rooms.bedroom()
-        # bedroom_door = items.door(self.player, "bedroom_key", living_room, bedroom)
-        # bedroom.doors = [bedroom_door]
+        bedroom = rooms.bedroom()
+        bedroom_door = items.door(self.player, "bedroom_key", living_room, bedroom)
+        bedroom.doors = [bedroom_door]
         
-        # office = rooms.office()
-        # office_door = items.door(self.player, "office key", living_room, office)
-        # office.doors = [office_door]
+        office = rooms.office()
+        office_door = items.door(self.player, "office key", living_room, office)
+        office.doors = [office_door]
         
-        # self.rooms = [living_room, kitchen, back_yard, bedroom, office]
+        living_room.doors = [kitchen_door, back_yard_door, bedroom_door, office_door]
+        
+        self.rooms = [living_room, kitchen, back_yard, bedroom, office]
         
         
         
