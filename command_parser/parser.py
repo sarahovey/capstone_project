0e9aa000e78e86c56abaa7dfaffc03c508b8206a
Daniel Silverman <daniel.silverman@me.com>
2019-06-04 20:12:08 -0700

    i think i got it

--- command_parser/parser.py
+++ command_parser/parser.py
@@ -1,14 +1,52 @@
-import items
-import npcs
-import rooms
+"""
+import files
+"""
+# import items
+# import npcs
+# import rooms
 
-command = input().split(" ")
+# command = input().split(" ")
 
-if command >= 4:
-    print("You don't know what you're doing.")
+# if command >= 4:
+#     print("You don't know what you're doing.")
 
-if command >= 3:
-    if command in items
+# if command >= 3:
+#     if command in items
 
-if command >= 2:
-    if 
+# if command >= 2:
+#     if 
+
+words = "here are words"
+
+words = words.split
+
+print("What will you do?")
+command = input()
+
+command = command.split()
+
+if len(command) > 4:
+    print("Too many!")
+elif len(command) == 4:
+    print("you tried too hard.... and fell over!")
+elif len(command) == 3:
+    action = command[0]
+    #range in for loop needs a slice notation that works to capture past the index of zero other wise eat will repeat twice
+    for word in range(len(command)):
+        object = command[word] + " "
+        
+    print("action:") 
+    print(action)
+    print("object: " )
+    print(object)
+    
+"""
+further actions
+"""
+#     game.player_interact(action, obj)
+    
+# elif len(command) == 2:
+#     #stuff i guess
+    
+# elif len(command) == 1:
+#     game.player_action(command)
