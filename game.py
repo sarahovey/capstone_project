8942d5686a65c2393965058ba343cddb7da2d6a0
Sara <sara.hovey@gmail.com>
2019-06-08 23:16:14 +0000

    flatten dir structure

--- game.py
+++ game.py
@@ -9,11 +9,10 @@ import items
 class Game:
     def __init__(self):
         self.phases_list = []
-        self.player = ""
-        
         self.player = player.Player()
         self.start_phases()
         
+    #Wake up all the phases    
     def start_phases(self):
         phase1 = phase.Phase1(self.player)
         phase2 = phase.Phase2(self.player)
@@ -35,41 +34,32 @@ class Game:
         message3 = "Are you ready to see your human?"
         end_description = "time to see your human!"
         end_door = items.phase_door(self.player, None, message3, "the end", end_description)
-        phase3.rooms[1].npcs[0].inventory.append(end_door)
+        phase3.rooms[1].npcs[0].inventory.append(end_door) #the ending door/cutscene
         
         self.phases_list = [phase1, phase2, phase3]
         
-        phase1.start_phase()
+        #changing this to help with testing, should be phase 1 here for real
+        phase2.start_phase()
+    
         
+    #if the first two words in a 3+ word command are "talk to"
+    def player_npc_interaction(self, npc):
+        self.player.interact_npc(npc)
+        
+    #if the first word in a 3+ word command is "open", "unlock", or "enter"
+    def player_door_interaction(self, door):
+        self.player.interact_door(door)
+        
+    #if the first word in a 2+ word command is "drop" or "use"
+    def player_inventory_interaction(self, action, object):
+        self.player.interact_inventory(action, object)
+     
+    #1 word commands, like help or inventory   
     def player_action(self, action):
         self.player.action(action)
         
-    def player_interaction(self, obj, action):
-        self.player.interact(object, action)
+    #2+ word command to interact with an item
+    def player_item_interaction(self, action, object):
+        self.player.interact_item(action, object)
         
-    def load_game(self):
-        print("Not yet!")
-        # print("are you sure?") #need to find a way to only show this if its being loaded from an in-progress game
-        # file_name = "saved_game/save_file"
-        # fp = open(file_name, "r")
-        
-        # #unpickle
-        # pickle.load(fp)
-        
-        # fp.close
-        
-    # def save_game(self):
-    #     print("Coming soon!")
-    #     # file_name = "saved_game/save_file"
-    #     # fp = open(file_name, "w")
-    #     # print("saving game...")
-    #     # pickle.dump(self, file_name)
-    #     # fp.close()
-        
-    # def new_game(self):
-    #     return "new game"
-    #     self.phases_list[0].start_phase(self.player)
-    #     #start a new phase
-        
-    
     
--- readme.md
+++ readme.md
@@ -1,9 +1,1 @@
-# CS467 - Capstone Project
-
-Team Members:
-Engine - Daniel Silverman
-Data - Sara Hovey
-Language Parser - Vannear Por
-
-To start running our adventure game enter the following command:
-    python data/demo.py
+# Data
